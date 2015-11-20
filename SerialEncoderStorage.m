a8e4c4e49972cb72343a3807208740fc9e1ac560
Tony Sun <tsun11@jhu.edu>
2015-11-20 15:34:16 -0500

    Initial MATLAB encoder function

---
+++ SerialEncoderStorage.m
@@ -0,0 +1,7 @@
+%for serial USB on left side of my Macbook 
+s = serial('/dev/tty.usbmodem1411');
+
+% create an array of xyz elements to store data
+EncoderPositionData = zeros(10000,1);
+
+datestr(now, 'dd-mm-yyyy HH:MM:SS FFF')
